9a55b37ab88fb1d562e5655aaa6045cd025e4a34
sa1tydonut <149614452+sa1tydonut@users.noreply.github.com>
2023-11-01 13:33:51 -0500

    Created files for xmas GitHub Page

---
+++ script.js
@@ -0,0 +1,9 @@
+function list() 
+{
+    alert ('You won\'t get any presents if you don\'t give us your list :(');
+}
+
+function gag()
+{
+    alert ('Did you really think I was going to go this deep into this?');
+}
